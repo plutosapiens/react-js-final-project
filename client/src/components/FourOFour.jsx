a5f768b35b16ee2728fae20139ef1e38b1d23bc1
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-11-12 21:48:44 +0200

    FP-Remove 404 text

--- client/src/components/FourOFour.jsx
+++ client/src/components/FourOFour.jsx
@@ -4,8 +4,6 @@ import styles from "./FourOFour.module.css"
 const FourOFour = () => {
   return (
         <div className={styles.main}>
-            {/* <div className={styles.cheekyText}>Tss... 404</div> */}
-
             <img className={styles.rectangle} alt="Image" src="/img/missing-beer.png" />
           <a href="#" className={styles.homeBtn}>Go back to home
           </a>
