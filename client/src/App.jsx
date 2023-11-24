c7b84bb04c8985467cb999fbab40f36487e690c3
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-11-24 22:04:23 +0200

    FP-Implement routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Index from "./components/Index"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -9,20 +10,31 @@ import Edit from "./components/Edit"
 import Catalog from "./components/Catalog"
 import Users from "./components/USers"
 
-import firebase from './utils/firebase'; // Import the Firebase configuration file
-
 
 
 function App() {
   return (
-    <>
+
+      <>
       <Header />
-      <Catalog />
-      <AddNew />
-      <Users />
+<BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/addnew" element={<AddNew />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<FourOFour />} /> {/* Catch-all route for 404 */}
+      </Routes>
+</BrowserRouter>
+
       <Footer />
-    </>
+      </>
   )
 }
+
 
 export default App
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
     return (
           <header className={styles.navbarWrapper}>
             <div className={styles.navbar}>
-              <a href="#" className={styles.navBtn}>Home</a>
-              <a href="#" className={styles.navBtn}>Users</a>
-              <a href="#" className={styles.navBtn}>Add beer</a>
-              <a href="#" className={styles.navBtn}>Catalog</a>
-              <a href="#" className={styles.navBtn}>Register</a>
-              <a href="#" className={styles.navBtn}>Login</a>
-              <a href="#" className={styles.navBtn}>Logout</a>
+              <a href="/" className={styles.navBtn}>Home</a>
+              <a href="/users" className={styles.navBtn}>Users</a>
+              <a href="/addnew" className={styles.navBtn}>Add beer</a>
+              <a href="/catalog" className={styles.navBtn}>Catalog</a>
+              <a href="/register" className={styles.navBtn}>Register</a>
+              <a href="/login" className={styles.navBtn}>Login</a>
+              <a href="/logout" className={styles.navBtn}>Logout</a>
             </div>
           </header>
     );
