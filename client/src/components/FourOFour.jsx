e52b8a81894dce514c330309d88f1d37818f4ee7
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-11-25 16:01:45 +0200

    FP-remove firebase

--- client/src/components/FourOFour.jsx
+++ client/src/components/FourOFour.jsx
@@ -5,8 +5,8 @@ const FourOFour = () => {
   return (
         <div className={styles.main}>
             <img className={styles.rectangle} alt="Image" src="/img/missing-beer.png" />
-          <a href="#" className={styles.homeBtn}>Go back to home
-          </a>
+
+          <a href="/" className={styles.homeBtn}>Go back to home</a>
         </div>
   );
 };
