640ed6097086966c09cbc4373175fc30312f6303
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-03 13:20:29 +0200

    FP - Implement add item function

--- client/src/components/AddNew/AddNew.jsx
+++ client/src/components/AddNew/AddNew.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom';
+
+import * as catalogService from '../../services/catalogService';
+
 import styles from "./AddNew.module.css"
 
 const AddNew = () => {
+  const navigate = useNavigate();
+
+  const createItemSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const itemData = Object.fromEntries(new FormData(e.currentTarget));
+
+    try{
+      await catalogService.create(itemData);
+
+      navigate('/catalog');
+    } catch (err) {
+      console.log(err)
+    }
+  }
      return (
         <div className={styles.main}>
-        <div className={styles.cheekyText}>New beer on the horizon</div>
+          <div className={styles.cheekyText}>New beer on the horizon</div>
 
-        <form method="POST">
-          <input className={styles.inputField} type="text" name="brand" id="AddNew-brand" placeholder="brand" />
-          <input className={styles.inputField} type="text" name="imgUrl" id="AddNew-imgUrl" placeholder="imgUrl" />
-          <input className={styles.inputField} type="text" name="taste" id="AddNew-taste" placeholder="taste" />
-          <input className={styles.inputField} type="text" name="strength" id="AddNew-strength" placeholder="strength" />
-          <input className={styles.inputField} type="text" name="description" id="AddNew-description" placeholder="description" />
-          <button className={styles.button} type="submit">AddNew</button>
-        </form>
+          <form id="create" onSubmit={createItemSubmitHandler} method="POST">
+            <input className={styles.inputField} type="text" name="brand" id="AddNew-brand" placeholder="brand" />
+            <input className={styles.inputField} type="text" name="imgUrl" id="AddNew-imgUrl" placeholder="imgUrl" />
+            <input className={styles.inputField} type="text" name="taste" id="AddNew-taste" placeholder="taste" />
+            <input className={styles.inputField} type="text" name="strength" id="AddNew-strength" placeholder="strength" />
+            <input className={styles.inputField} type="text" name="description" id="AddNew-description" placeholder="description" />
+            <button className={styles.button} type="submit">AddNew</button>
+          </form>
         </div>
         
   );
