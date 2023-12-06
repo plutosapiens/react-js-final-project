476166bf0ddf7d82698ea35e211addb24d82304f
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-06 12:37:46 +0200

    FP - Start using data instead of jsonstore collections

--- client/src/services/catalogService.js
+++ client/src/services/catalogService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/items';
+const baseUrl = 'http://localhost:3030/data/items';
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    return result;
 };
 
 export const getOne = async (itemId) => {
