724569d61028c81cb952a008608e4caa1dead867
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-07 12:55:14 +0200

    FP - Add html and css to details

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -8,8 +8,6 @@ import styles from "./Details.module.css";
 const Details = () => {
   const [item, setItem] = useState({});
   const { itemId } = useParams();
-  console.log('Item ID:', itemId);
-
   
   useEffect(() => {
     if (itemId) {
@@ -21,14 +19,27 @@ const Details = () => {
       // Handle the case where itemId is undefined (e.g., show a message or render default content)
     }
   }, [itemId]);
+
+  const likeItem = () => {
+    // Implement logic for like functionality here
+    // For example, update a like count in the database or toggle like status
+    // This function will be called when the like button is clicked
+  };
+  console.log(item)
   
-
-     return (
-        <div className={styles.main}>
-        <div className={styles.cheekyText}>Item:{item.name}</div>
-
-        </div>
-        
+  return (
+    <div className={styles.main}>
+      <img src={item.imgUrl} alt={item.name} className={styles.image} />
+      <div className={styles.details}>
+        <div className={styles.cheekyText}>Item: {item.name}</div>
+        <p className={styles.description}>Description: {item.description}</p>
+        <p className={styles.description}>Yarn Weight: {item.yarnWeight}</p>
+        <p className={styles.description}>Needle Size: {item.needleSize}</p>
+        <button onClick={likeItem} className={styles.button}>
+          Like
+        </button>
+      </div>
+    </div>
   );
 };
 
