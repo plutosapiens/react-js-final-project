dd84cd1da9aebca392484732f40fd423f7bafc58
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-07 15:42:16 +0200

    FP - implement only one like per user on item

--- client/src/components/Details/Like/Like.jsx
+++ client/src/components/Details/Like/Like.jsx
@@ -1,27 +1,47 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect  } from 'react';
 import { useParams } from 'react-router-dom';
 
 import  AuthContext  from '../../../contexts/authContext';
 import styles from './Like.module.css';
 import * as likeService from '../../../services/likeService'
-import { likeContext, likeContextProvider } from '../../../contexts/likeContext';
 
 function LikeButton() {
   const { email } = useContext(AuthContext)
   const [liked, setLiked] = useState(false);
   const { itemId } = useParams();
 
+  useEffect(() => {
+    const checkUserLiked = async () => {
+      try {
+        const likes = await likeService.getAll(); // Fetch all likes
+        const userLikedItem = likes.find(
+          (like) => like.email === email && like.itemId === itemId
+        );
+
+        if (userLikedItem) {
+          setLiked(true); // If the user liked this item, set liked state to true
+        }
+      } catch (error) {
+        console.error('Error checking user liked item:', error);
+      }
+    };
+
+    checkUserLiked();
+  }, [email, itemId]);
   
   const likeHandler = async () => {
-    try {
-      const data = {itemId, email}
+    if (!liked) {
+      try {
+        const data = { itemId, email };
         await likeService.create(data);
+        setLiked(true);
         // Perform any additional logic upon successful like
-    } catch (error) {
+      } catch (error) {
         console.error('Error liking item:', error);
         // Handle error cases
+      }
     }
-};
+  };
   
   return (
     <button
