37d4eb77b7b617378173242ebe08a3b7853bd24a
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-08 10:50:08 +0200

    FP - show names when opening the edit page

--- client/src/components/Edit/Edit.jsx
+++ client/src/components/Edit/Edit.jsx
@@ -1,16 +1,77 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-import * as catalogService from '../../services/catalogService';
-import useForm from "../../hooks/useForm";
 import styles from "./Edit.module.css"
 
 
 
 const Edit = () => {
+  const [name, setName] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [yarnWeight, setYarnWeight] = useState('');
+  const [needleSize, setNeedleSize] = useState('');
+  const [description, setDescription] = useState('');
+  
+  useEffect(() => {
+    setName(localStorage.getItem('Name'));
+    setImgUrl(localStorage.getItem('ImgUrl'));
+    setYarnWeight(localStorage.getItem('YarnWeight'));
+    setNeedleSize(localStorage.getItem('NeedleSize'));
+    setDescription(localStorage.getItem('Description'));
+  })
 
      return (
-      <div>Edit menu</div>
+<div className={styles.main}>
+          <div className={styles.cheekyText}>New beer on the horizon</div>
+
+          <form id="create">
+            <input 
+              className={styles.inputField}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              className={styles.inputField}
+              type="text"
+              name="imgUrl"
+              id="imgUrl"
+              placeholder="imgUrl"
+              value={imgUrl}
+              onChange={(e) => setImgUrl(e.target.value)}
+            />
+            <input
+              className={styles.inputField}
+              type="text"
+              name="yarnWeight"
+              id="yarnWeight"
+              placeholder="yarn weight"
+              value={yarnWeight}
+              onChange={(e) => setYarnWeight(e.target.value)}
+            />
+            <input
+              className={styles.inputField}
+              type="text"
+              name="needleSize"
+              id="needleSize"
+              placeholder="needle size"
+              value={needleSize}
+              onChange={(e) => setNeedleSize(e.target.value)}
+            />
+            <input
+              className={styles.inputField}
+              type="text"
+              name="description"
+              id="description"
+              placeholder="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <button className={styles.button} type="submit">AddNew</button>
+          </form>
+        </div>
         
   );
 };
