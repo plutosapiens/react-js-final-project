2bcecec481d4d23fdb52a30e75b27603e3c3c55f
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-08 15:19:40 +0200

    FP -Copy catalog component

---
+++ client/src/components/Catalog copy/Catalog.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import styles from "./Catalog.module.css"
+import CatalogItem from "./CatalogItem/CatalogItem";
+
+import * as catalogService from "../../services/catalogService";
+
+const Catalog = () => {
+    const [items, setCatalog] = useState([]);
+
+    useEffect(() => {
+        catalogService.getAll()
+        .then(result => setCatalog(result));
+    }, []);
+
+     return (
+        <div className={styles.main}>
+            <div className={styles.cheekyText}>
+                They’re fresh
+                <br />
+                They’re excited</div>
+
+            <div className={styles.catalog}>
+                {items.map(item => (
+                   <CatalogItem 
+                    key={item._id} {...item} />
+               ))}
+            </div>
+        </div>
+        
+  );
+};
+
+export default Catalog;
