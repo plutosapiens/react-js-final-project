bba328c94929f59fcc2a2697e9aeaacd389bfa94
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-08 16:58:19 +0200

    FP - Add username to index page

--- client/src/components/Index/Index.jsx
+++ client/src/components/Index/Index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useContext } from "react";
 
 import styles from "./Index.module.css"; 
 import withAuth from "../../HOC/withAurh";
+import AuthContext from "../../contexts/authContext";
 
 const Index = () => {
+  const { isAuthenticated, username } = useContext(AuthContext)
   return (
         <div className={styles.main}>
           <div className={styles.cheekyText}>
@@ -12,11 +14,10 @@ const Index = () => {
             Knit Wit Central
           </div>
           <p className={styles.p}>
-            Lorem ipsum
+          {isAuthenticated ? `Hello, ${username}!` : 'Friend'}
             <br />
             <br />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae molestie leo. Curabitur at convallis arcu,
-            et aliquam ex. Integer imperdiet diam at purus consequat rhoncus.
+            Our platform is a cozy corner of the internet where knitting enthusiasts gather to share patterns, showcase handmade creations, exchange tips and tricks, and celebrate the joy of crafting with needles and threads. 
           </p>
           <img className={styles.rectangle} alt="Rectangle" src="/img/rectangle-2.png" />
         </div>
