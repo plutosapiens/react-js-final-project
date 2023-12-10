c616aaa912a660487a978e5bc5f6ac152a7b67d6
Pluto <96972840+plutosapiens@users.noreply.github.com>
2023-12-10 16:26:53 +0200

    FP - Implement validation on login and register

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,6 +1,7 @@
-import { createContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
+import { registerValidation, loginValidation } from '../utils/validation';
 import * as authService from '../services/authService';
 import usePersistedState from '../hooks/usePersistedState';
 import Paths from "../paths";
@@ -11,16 +12,38 @@ export const AuthProvider = ({ children }) => {
 
     const navigate = useNavigate();
     const [auth, setAuth] = usePersistedState('auth', {});
+    const [errors, setErrors] = useState();
+
     const loginSubmitHandler = async (values) => {
+      try{
+        setErrors({});
+        const validationErrors = loginValidation(values);
+
+        if(Object.keys(validationErrors).length > 0) {
+          throw validationErrors;
+        }
+
         const result = await authService.login(values.email, values.password);
         
         setAuth(result);
     
         localStorage.setItem('accessToken', result.accessToken);
         navigate(Paths.Home);
-      };
+      }catch(error) {
+        console.error('Login failed:', error);
+      }
+    }
     
       const registerSubmitHandler = async (values) => {
+        try{
+          setErrors({});
+          const validationErrors = registerValidation(values);
+
+          if(Object.keys(validationErrors).length > 0){
+            throw validationErrors
+            // return setErrors(state => ({...state, validationErrors}));
+          }
+
         const result = await authService.register(
           values.email,
           values.password,
@@ -32,7 +55,15 @@ export const AuthProvider = ({ children }) => {
     
           localStorage.setItem('accessToken', result.accessToken);
           navigate(Paths.Home)
+
+          if(!Response.ok) {
+            setErrors(state => ({...state, registerError: Response.message}));
+          }
       }
+    catch(error) {
+      console.error('Register failed:', error);
+    }
+  }
     
       const logoutHandler = () => {
         setAuth({});
@@ -48,6 +79,7 @@ export const AuthProvider = ({ children }) => {
         userId: auth._id,
         isAuthenticated: !!auth.accessToken,
       };
+
     return (
         <AuthContext.Provider value={values}>
             {children}
